3106440ad81d8f56fbfc7df63a89f5fbf5dbe9e6
Oguzhan NAMLICI <oguzhannamlici@gmail.com>
2022-05-07 17:10:08 +0300

    Added basic project in this repo

---
+++ calculating-hypotenuse/src/Main.java
@@ -0,0 +1,23 @@
+import jdk.jshell.spi.SPIResolutionException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int firstEdge,secondEdge,thirdEdge;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first edge: ");
+        firstEdge = scanner.nextInt();
+        System.out.print("Enter second edge: ");
+        secondEdge = scanner.nextInt();
+        System.out.print("Enter third edge: ");
+        thirdEdge = scanner.nextInt();
+
+        double perimeterofTriangle = ((firstEdge+secondEdge+thirdEdge)/2);
+        System.out.println("Perimeter of Triangle: " + perimeterofTriangle);
+        double areaOfTriangle = Math.sqrt(perimeterofTriangle*(perimeterofTriangle-firstEdge)*(perimeterofTriangle-secondEdge)*(perimeterofTriangle-thirdEdge));
+        System.out.println("Area of Triangle: "+areaOfTriangle);
+    }
+}
