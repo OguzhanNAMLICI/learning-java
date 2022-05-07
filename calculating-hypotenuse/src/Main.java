516ed4424b1982a57a9bc2ce5e2ab5dfb920cffd
Oguzhan NAMLICI <oguzhannamlici@gmail.com>
2022-05-07 17:15:56 +0300

    added some code in this repo

--- calculating-hypotenuse/src/Main.java
+++ calculating-hypotenuse/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int firstEdge,secondEdge,thirdEdge;
-
+        double hypotenuse;
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first edge: ");
@@ -15,9 +15,17 @@ public class Main {
         System.out.print("Enter third edge: ");
         thirdEdge = scanner.nextInt();
 
+
+
         double perimeterofTriangle = ((firstEdge+secondEdge+thirdEdge)/2);
         System.out.println("Perimeter of Triangle: " + perimeterofTriangle);
         double areaOfTriangle = Math.sqrt(perimeterofTriangle*(perimeterofTriangle-firstEdge)*(perimeterofTriangle-secondEdge)*(perimeterofTriangle-thirdEdge));
         System.out.println("Area of Triangle: "+areaOfTriangle);
+
+
+        hypotenuse = Math.sqrt((firstEdge*firstEdge)+(secondEdge*secondEdge));
+        System.out.println("Hypotenuse: " + hypotenuse);
+
+
     }
 }
